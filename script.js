922f38cf00e3e581dafe36081340f4d1ce59c6ea
LITHIKA L G <lglithika3@gmail.com>
2026-08-12 11:11:03 +0530

    Day 8: Finalized Projects section

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-<<<<<<< HEAD
+/* ================================
+CONTACT FORM
+================================ */
+
 const contactForm = document.getElementById("contactForm");
 
 contactForm.addEventListener("submit", function(event) {
+
+```
 event.preventDefault();
 
 const name = document.getElementById("name").value;
@@ -13,12 +18,19 @@ alert(
 );
 
 contactForm.reset();
+```
 
 });
+
+/* ================================
+SCROLL TO TOP BUTTON
+================================ */
+
 const scrollBtn = document.getElementById("scrollBtn");
 
 window.addEventListener("scroll", function() {
 
+```
 if (window.scrollY > 300) {
 
     scrollBtn.style.display = "block";
@@ -28,40 +40,17 @@ if (window.scrollY > 300) {
     scrollBtn.style.display = "none";
 
 }
+```
 
 });
 
 scrollBtn.addEventListener("click", function() {
+
+```
 window.scrollTo({
     top: 0,
     behavior: "smooth"
 });
+```
 
 });
-=======
-document.getElementById("scrollBtn").addEventListener("click", function () {
-    document.getElementById("projects").scrollIntoView({
-        behavior: "smooth"
-    });
-});
-const modal = document.getElementById("modal");
-const modalTitle = document.getElementById("modalTitle");
-const modalDescription = document.getElementById("modalDescription");
-const closeBtn = document.getElementById("close");
-const buttons = document.querySelectorAll(".details-btn");
-buttons.forEach(function(button){
-    button.addEventListener("click", function(){
-        modal.style.display = "block";
-        modalTitle.innerText = this.dataset.title;
-        modalDescription.innerText = this.dataset.description;
-    });
-});
-closeBtn.onclick = function(){
-    modal.style.display = "none";
-}
-window.onclick = function(event){
-    if(event.target == modal){
-        modal.style.display = "none";
-    }
-}
->>>>>>> e3a8e4a2bf47888d949602bbffc6e11ce54b98fc
