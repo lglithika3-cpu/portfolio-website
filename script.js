7af02d070d5ad902066e70346c2bff94c266b1a1
LITHIKA L G <lglithika3@gmail.com>
2026-08-12 19:58:17 +0530

    Day 12: Deploy portfolio with GitHub Pages

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const scrollBtn=document.getElementById("scrollBtn");
-
 window.addEventListener("scroll",()=>{
 if(window.scrollY>300){
 scrollBtn.style.display="block";
@@ -7,32 +6,24 @@ scrollBtn.style.display="block";
 scrollBtn.style.display="none";
 }
 });
-
 scrollBtn.addEventListener("click",()=>{
 window.scrollTo({
 top:0,
 behavior:"smooth"
 });
 });
-
 document.querySelectorAll('a[href^="#"]').forEach(link=>{
 link.addEventListener("click",event=>{
 const target=document.querySelector(link.getAttribute("href"));
-
 if(target){
 event.preventDefault();
-
 target.scrollIntoView({
 behavior:"smooth"
 });
 }
 });
 });
-
-const cards=document.querySelectorAll(
-".skill-card,.project-card,.education-card"
-);
-
+const cards=document.querySelectorAll(".skill-card,.project-card,.education-card");
 const observer=new IntersectionObserver(entries=>{
 entries.forEach(entry=>{
 if(entry.isIntersecting){
@@ -41,11 +32,17 @@ entry.target.style.transform="translateY(0)";
 }
 });
 },{threshold:0.15});
-
 cards.forEach(card=>{
 card.style.opacity="0";
 card.style.transform="translateY(40px)";
 card.style.transition="opacity .7s ease,transform .7s ease";
-
 observer.observe(card);
+});
+const contactForm=document.getElementById("contactForm");
+const formMessage=document.getElementById("formMessage");
+contactForm.addEventListener("submit",event=>{
+event.preventDefault();
+formMessage.textContent="Thank you! Your message has been submitted successfully.";
+formMessage.style.color="#910b5b";
+contactForm.reset();
 });
